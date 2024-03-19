925bbb564cc6ca725e80cda6ec38761574cf4f08
Jose <jgh9094@gmail.com>
2024-03-19 11:59:31 -0700

    fix

--- Data-Tools/Collect/best-performance.py
+++ Data-Tools/Collect/best-performance.py
@@ -7,19 +7,21 @@ import pandas as pd
 replicates = 50
 lexicase_pop_size = {'Pop_50': 50 ,'Pop_100': 100 ,'Pop_500': 500 ,'Pop_1000': 1000 ,'Pop_5000': 5000}
 
-
 def BestPerformance(file_name):
     # create pandas data frame of entire csv
     df = pd.read_csv(file_name)
     return df['performance'].max()
 
-def CheckDir(dir,exp):
+def CheckDir(dir,exp,dump):
     # check if data dir exists
     if not os.path.isdir(dir):
         sys.exit('DATA DIRECTORY DOES NOT EXIST')
 
+    best_performance = []
+    pop_size_l = []
+
     # Iterating through both keys and values
-    for pop_size, tracker in lexicase_pop_size.items():
+    for pop_size, size in lexicase_pop_size.items():
         # experiment dir
         exp_dir = dir + pop_size + '/' + exp
 
@@ -36,24 +38,27 @@ def CheckDir(dir,exp):
 
             file_dir = seed_dir + '/data.csv'
 
-            # record best performance
-            performance = BestPerformance(file_dir)
-            print('performance:', performance)
+            # record data
+            best_performance.append(BestPerformance(file_dir))
+            pop_size_l.append(size)
 
+    df = pd.DataFrame({'performance': pd.Series(best_performance), 'pop_size': pd.Series(pop_size_l)})
+    df.to_csv(path_or_buf=dump+'best.csv', index=False)
 
 def main():
     # read in arguements
     parser = argparse.ArgumentParser()
     # where to save the results/models
     parser.add_argument("-d", "--data_dir", default="./", type=str)
+    parser.add_argument("-du", "--dump_dir", default="./", type=str)
 
     args = parser.parse_args()
     print('data_dir:', args.data_dir)
     print()
 
-    print('CHECKING RUNS NOW!')
-    CheckDir(args.data_dir, 'Exploitation/')
-    print('FINISHED CHECKING!')
+    print('GETTING BEST PERFORMANCES!')
+    CheckDir(args.data_dir, 'Exploitation/', args.dump_dir)
+    print('FINISHED GATHERING PERFORMANCES!')
 
 if __name__ == '__main__':
     main()
