edb3b83f9301e956cb0270afcee6798a86e7cc53
Jose <jgh9094@gmail.com>
2024-03-19 11:18:28 -0700

    fix

--- Data-Tools/Checker/checker.py
+++ Data-Tools/Checker/checker.py
@@ -92,7 +92,7 @@ def CheckDir(dir,exp):
     print()
     for pop_size, tracker in lexicase_success.items():
         print(pop_size,':',len(tracker))
-        print('seeds:',tracker.sort())
+        print('seeds:',tracker)
         print('does_not_exist:', len(does_not_exist[pop_size]))
         print('does_not_exist:', does_not_exist[pop_size])
         print('evals_not_met:', len(evals_not_met[pop_size]))
